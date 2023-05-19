d6e93e2a9967db765db5a0d0184450bc189a9a2b
Farrell Naufal <101353709+farnoovv@users.noreply.github.com>
2023-05-19 13:00:49 +0700

    Landing Page UI

---
+++ resources/views/card.blade.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Freelancer - Start Bootstrap Theme</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <div class="container py-5">
+            <!-- For Demo Purpose-->
+            <header class="text-center mb-5">
+              <h1 class="display-4 font-weight-bold">Bootstrap Cards</h1>
+              <p class="font-italic text-muted mb-0">An awesome Bootstrap 4 cards collection with variant content.</p>
+              <p class="font-italic text-muted">Snippet By <a href="https://bootstrapious.com" class="text-muted">
+                  <u>Bootstrapious</u></a>
+              </p>
+            </header>
+          
+          
+            <!-- First Row [Prosucts]-->
+            <h2 class="font-weight-bold mb-2">Trending</h2>
+            <p class="font-italic text-muted mb-4">Explore top seller games this week!</p>
+          
+            <div class="row pb-5 mb-4">
+              <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                <!-- Card-->
+                <div class="card rounded shadow-sm border-0">
+                  <div class="card-body p-4"><img src="assets/img/product1.png" alt="" class="img-fluid d-block mx-auto mb-3">
+                    <h5> <a href="#" class="text-dark">A Space for the Unbound</a></h5>
+                    <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <ul class="list-inline small">
+                      <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                      <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                      <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                      <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                      <li class="list-inline-item m-0"><i class="fa fa-star-o text-success"></i></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+          
+              <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                <!-- Card-->
+                <div class="card rounded shadow-sm border-0">
+                  <div class="card-body p-4"><img src="assets/img/product2.png" alt="" class="img-fluid d-block mx-auto mb-3">
+                    <h5> <a href="#" class="text-dark">Coffee Talk</a></h5>
+                    <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <ul class="list-inline small">
+                      <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                      <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                      <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                      <li class="list-inline-item m-0"><i class="fa fa-star-o text-success"></i></li>
+                      <li class="list-inline-item m-0"><i class="fa fa-star-o text-success"></i></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+          
+              <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                <!-- Card-->
+                <div class="card rounded shadow-sm border-0">
+                  <div class="card-body p-4"><img src="assets/img/product3.png" alt="" class="img-fluid d-block mx-auto mb-3">
+                    <h5> <a href="#" class="text-dark">Coral Island</a></h5>
+                    <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <ul class="list-inline small">
+                      <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                      <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                      <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                      <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                      <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+          
+              <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                <!-- Card-->
+                <div class="card rounded shadow-sm border-0">
+                  <div class="card-body p-4"><img src="assets/img/product4.png" alt="" class="img-fluid d-block mx-auto mb-3">
+                    <h5> <a href="#" class="text-dark">Troublemaker</a></h5>
+                    <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <ul class="list-inline small">
+                      <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                      <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                      <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                      <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                      <li class="list-inline-item m-0"><i class="fa fa-star-o text-success"></i></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          
+          
+            <!-- Second Row [Team]-->
+            <h2 class="font-weight-bold mb-2">Our Team</h2>
+            <p class="font-italic text-muted mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+          
+            <div class="row pb-5 mb-4">
+              <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                <!-- Card-->
+                <div class="card shadow-sm border-0 rounded">
+                  <div class="card-body p-0"><img src="https://bootstrapious.com/i/snippets/sn-cards/profile-1_dewapk.jpg" alt="" class="w-100 card-img-top">
+                    <div class="p-4">
+                      <h5 class="mb-0">Mark Rockwell</h5>
+                      <p class="small text-muted">CEO - Consultant</p>
+                      <ul class="social mb-0 list-inline mt-3">
+                        <li class="list-inline-item m-0"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+                        <li class="list-inline-item m-0"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                        <li class="list-inline-item m-0"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                        <li class="list-inline-item m-0"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+          
+              <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                <!-- Card-->
+                <div class="card shadow-sm border-0 rounded">
+                  <div class="card-body p-0"><img src="https://bootstrapious.com/i/snippets/sn-cards/profile-3_ybnq8v.jpg" alt="" class="w-100 card-img-top">
+                    <div class="p-4">
+                      <h5 class="mb-0">Mark Rockwell</h5>
+                      <p class="small text-muted">CEO - Consultant</p>
+                      <ul class="social mb-0 list-inline mt-3">
+                        <li class="list-inline-item m-0"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+                        <li class="list-inline-item m-0"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                        <li class="list-inline-item m-0"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                        <li class="list-inline-item m-0"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+          
+              <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                <!-- Card-->
+                <div class="card shadow-sm border-0 rounded">
+                  <div class="card-body p-0"><img src="https://bootstrapious.com/i/snippets/sn-cards/profile-2_ujssbj.jpg" alt="" class="w-100 card-img-top">
+                    <div class="p-4">
+                      <h5 class="mb-0">Mark Rockwell</h5>
+                      <p class="small text-muted">CEO - Consultant</p>
+                      <ul class="social mb-0 list-inline mt-3">
+                        <li class="list-inline-item m-0"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+                        <li class="list-inline-item m-0"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                        <li class="list-inline-item m-0"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                        <li class="list-inline-item m-0"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+          
+              <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                <!-- Card-->
+                <div class="card shadow-sm border-0 rounded">
+                  <div class="card-body p-0"><img src="https://res.cloudinary.com/mhmd/image/upload/v1570799922/profile-2_ujssbj.jpg" alt="" class="w-100 card-img-top">
+                    <div class="p-4">
+                      <h5 class="mb-0">Mark Rockwell</h5>
+                      <p class="small text-muted">CEO - Consultant</p>
+                      <ul class="social mb-0 list-inline mt-3">
+                        <li class="list-inline-item m-0"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+                        <li class="list-inline-item m-0"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                        <li class="list-inline-item m-0"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                        <li class="list-inline-item m-0"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          
+            <!-- Third Row [Profiles]-->
+            
+          
+              
+          
+          
+              
+            </div>
+          
+    
+            </div>
+          </div> <!-- Navigation-->
+        
+    </body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Freelancer - Start Bootstrap Theme</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/LOGO.png" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top" class="">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand" href="#page-top"> <img src="assets/img/LOGO.png" alt=""></a>
+                <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#portfolio">Explore</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#about">Store</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#contact">About</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead  bg-primary text-white text-center rounded-2" >
+            <div class="container d-flex align-items-center flex-column">
+                <!-- Masthead Avatar Image-->
+                <img class="masthead-avatar mb-5 img-fluid " src="assets/img/banner1.png" style="width:1200px" alt="..." />
+            </div>
+        </header>
+        <div class="mx-lg-5 my-lg-5">
+        <!-- First Row [Prosucts]-->
+        <h2 class="font-weight-bold mb-2">Trending</h2>
+        <p class="font-italic text-muted mb-4">Explore top seller games this week!</p>
+      
+        <div class="row pb-5 mb-4">
+          <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+            <!-- Card-->
+            <div class="card rounded shadow-sm border-0">
+              <div class="card-body p-4"><img src="assets/img/product1.png" alt="" class="img-fluid d-block mx-auto mb-3">
+                <h5> <a href="#" class="text-dark">A Space for the Unbound</a></h5>
+                <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                <ul class="list-inline small">
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star-o text-success"></i></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+      
+          <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+            <!-- Card-->
+            <div class="card rounded shadow-sm border-0">
+              <div class="card-body p-4"><img src="assets/img/product2.png" alt="" class="img-fluid d-block mx-auto mb-3">
+                <h5> <a href="#" class="text-dark">Coffee Talk</a></h5>
+                <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                <ul class="list-inline small">
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star-o text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star-o text-success"></i></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+      
+          <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+            <!-- Card-->
+            <div class="card rounded shadow-sm border-0">
+              <div class="card-body p-4"><img src="assets/img/product3.png" alt="" class="img-fluid d-block mx-auto mb-3">
+                <h5> <a href="#" class="text-dark">Coral Island</a></h5>
+                <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                <ul class="list-inline small">
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+      
+          <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+            <!-- Card-->
+            <div class="card rounded shadow-sm border-0">
+              <div class="card-body p-4"><img src="assets/img/product4.png" alt="" class="img-fluid d-block mx-auto mb-3">
+                <h5> <a href="#" class="text-dark">Troublemaker</a></h5>
+                <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                <ul class="list-inline small">
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star-o text-success"></i></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      
+      
+       
+        <h2 class="font-weight-bold mb-2">Sale</h2>
+        <p class="font-italic text-muted mb-4">Buy Discounted Games!</p>
+      
+        <div class="row pb-5 mb-4">
+          <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+            <!-- Card-->
+            <div class="card rounded shadow-sm border-0">
+              <div class="card-body p-4"><img src="assets/img/product1.png" alt="" class="img-fluid d-block mx-auto mb-3">
+                <h5> <a href="#" class="text-dark">A Space for the Unbound</a></h5>
+                <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                <ul class="list-inline small">
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star-o text-success"></i></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+      
+          <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+            <!-- Card-->
+            <div class="card rounded shadow-sm border-0">
+              <div class="card-body p-4"><img src="assets/img/product2.png" alt="" class="img-fluid d-block mx-auto mb-3">
+                <h5> <a href="#" class="text-dark">Coffee Talk</a></h5>
+                <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                <ul class="list-inline small">
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star-o text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star-o text-success"></i></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+      
+          <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+            <!-- Card-->
+            <div class="card rounded shadow-sm border-0">
+              <div class="card-body p-4"><img src="assets/img/product3.png" alt="" class="img-fluid d-block mx-auto mb-3">
+                <h5> <a href="#" class="text-dark">Coral Island</a></h5>
+                <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                <ul class="list-inline small">
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+      
+          <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+            <!-- Card-->
+            <div class="card rounded shadow-sm border-0">
+              <div class="card-body p-4"><img src="assets/img/product4.png" alt="" class="img-fluid d-block mx-auto mb-3">
+                <h5> <a href="#" class="text-dark">Troublemaker</a></h5>
+                <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                <ul class="list-inline small">
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                  <li class="list-inline-item m-0"><i class="fa fa-star-o text-success"></i></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        <!-- Footer-->
+        <footer class="footer text-center">
+            <div class="container">
+                <div class="row">
+                    <!-- Footer Location-->
+                    <div class="col-lg-4 mb-5 mb-lg-0">
+                        <h4 class="text-uppercase mb-4">Location</h4>
+                        <p class="lead mb-0">
+                            2215 John Daniel Drive
+                            <br />
+                            Clark, MO 65243
+                        </p>
+                    </div>
+                    <!-- Footer Social Icons-->
+                    <div class="col-lg-4 mb-5 mb-lg-0">
+                        <h4 class="text-uppercase mb-4">Around the Web</h4>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-facebook-f"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-twitter"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-linkedin-in"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-dribbble"></i></a>
+                    </div>
+                    <!-- Footer About Text-->
+                    <div class="col-lg-4">
+                        <h4 class="text-uppercase mb-4">About Freelancer</h4>
+                        <p class="lead mb-0">
+                            Freelance is a free to use, MIT licensed Bootstrap theme created by
+                            <a href="http://startbootstrap.com">Start Bootstrap</a>
+                            .
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- Copyright Section-->
+        <div class="copyright py-4 text-center text-white">
+            <div class="container"><small>Copyright &copy; Your Website 2023</small></div>
+        </div>
+        <!-- Portfolio Modals-->
+        <!-- Portfolio Modal 1-->
+        
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
