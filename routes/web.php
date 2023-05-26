72f2d84024468334fdbdf12982714c04f7223d3b
Farrell Naufal <101353709+farnoovv@users.noreply.github.com>
2023-05-26 08:05:56 +0700

    Layouting Navbar and add store and explore page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/explore', function () {
+    return view('explore');
+});
+Route::get('/store', function () {
+    return view('store');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/login', function () {
+    return view('login');
+});
